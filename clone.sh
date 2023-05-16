08a99317225133284b987557eb4256b31b5a61a0
Simone Sestito <simone@simonesestito.com>
2023-05-16 12:34:22 +0200

    Clone destination repo if not done yet

--- clone.sh
+++ clone.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
-set -x
 
 # TODO: Environment variables to change!
 export OVERLEAF_READ_SHARE_URL='https://www.overleaf.com/read/xdjkjrjcqyym'
 export PROJECT_FOLDER='/home/simone/overleaf-commit/bachelor-thesis'
+export GIT_REPO_URL='git@github.com:simonesestito/bachelor-thesis.git'
 export ZIP_PROJECT='Bachelor Thesis.zip' # Must be the same Overleaf project name
 
 # Download project and clone to GitHub
 
 cd "$(dirname "$0")"
+
+# Clone if doesn't exist yet
+if [ ! -d "$PROJECT_FOLDER/.git/" ]; then
+    git clone "$GIT_REPO_URL" "$PROJECT_FOLDER"
+fi
 
 # Pull
 (
